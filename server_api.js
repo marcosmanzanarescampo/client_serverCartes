1572e8c82e828ff518348d6d1bf176d0d856fb83
Marcos Manzanares <marcosmanzanarescampo@gmail.com>
2025-02-25 15:38:41 +0100

    Add files via upload

--- server_api.js
+++ server_api.js
@@ -1,5 +1,12 @@
 const http = require("http");
+const { log } = require("util");
 const fs = require("fs").promises;
+
+// extraction de l'élement à éliminer/modifier d'un URL
+const takeElement = (url) => {
+    const element = url.substring(url.lastIndexOf('/') + 1);
+    return element;
+}
 
 // Lecture du fichier JSON
 async function readArticles() {
@@ -29,6 +36,7 @@ const server = http.createServer(async (req, res) => {
     console.log(`${new Date().toISOString()} - ${req.method} ${req.url}`);
 
     // Routes API
+        //   ********************************************************************************* GET *****************************************
     if (req.url === "/articles" && req.method === "GET") {
       console.log('Serving GET...');
       
@@ -53,12 +61,100 @@ const server = http.createServer(async (req, res) => {
                 res.end(JSON.stringify({ error: "Invalid data" }));
             }
         });
+            //   ********************************************************************************* GET *****************************************
     } else if (req.url.match(/\/articles\/\d+$/) && req.method === "DELETE") {      
       //Code
+    //   ********************************************************************************* DELETE *****************************************
+// 1. Lire tous les articles
+// 2. Remplacer l'ancien article par le nouveau article
+// 3. Ecrire la liste d'articles de nouveau dans le fichier
+
+            // 1
+            const articles = await readArticles();            
+            let newArticles = [];
+            const elementAEliminer = takeElement(req.url);
+            console.log(`èlement à éliminer ${elementAEliminer}`);
+            
+
+            // 2
+
+            articles.forEach(element => {
+
+                console.log(`${element.id} = ${elementAEliminer}:`);
+
+                if(parseInt(element.id) === parseInt(elementAEliminer)){  
+                }
+                else{
+                    newArticles.push(element);                 
+                }                
+            }); 
+            
+            console.log(newArticles);
+            
+            // 3
+            await writeArticles(newArticles);
+
+            res.writeHead(201, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(newArticles));
+
+    //   ********************************************************************************* DELETE *****************************************
       console.log('Serving DELETE...');
     } else if (req.url.match(/\/articles\/\d+$/) && req.method === "PUT") {
+      //   ********************************************************************************* PUT *****************************************
       //Code
       console.log('Serving PUT...');
+    // 1. Recuperer l'element à modifier
+    // 2. Le remplacer
+    // 3. Recrire le fichier 
+
+    // 1
+    let body = "";
+    req.on("data", (chunk) => (body += chunk));
+    req.on("end", async () => {
+        try {
+
+            const elementAModifier = JSON.parse(body);
+
+            // 1. Lire tous les articles
+            // 2. Remplacer l'ancien article par le nouveau article
+            // 3. Ecrire la liste d'articles de nouveau dans le fichier
+            
+            // 1
+            const articles = await readArticles();            
+            let newArticles = [];
+
+            // 2
+
+            articles.forEach(element => {
+
+                console.log(`${element.id} = ${elementAModifier.id}:`);
+
+                if(parseInt(element.id) === parseInt(elementAModifier.id)){  
+                    console.log('including nouveau...');                                      
+                    newArticles.push(elementAModifier);
+                }
+                else{
+                    console.log('including ancien...');
+                    newArticles.push(element);                 
+                }                
+            }); 
+            
+            console.log(newArticles);
+            
+            // 3
+            await writeArticles(newArticles);
+
+            res.writeHead(201, { "Content-Type": "application/json" });
+            res.end(JSON.stringify(newArticles));
+        } catch (error) {
+            res.writeHead(400);
+            res.end(JSON.stringify({ error: "Invalid data" }));
+        }
+    });
+    
+      //   ********************************************************************************* PUT *****************************************
+
+
     } else {
         res.writeHead(404);
         res.end(JSON.stringify({ error: "Not found" }));
